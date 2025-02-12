ab47a2c5ebeba2fb5ea5083a69a0f9a12ca3df6a
LAB13_USER ON WS02 <L13X13W02@citulab.net>
2025-02-12 13:09:28 +0800

    init: springbooot

---
+++ backend/src/main/java/edu/cit/tooltrack/controller/sample.java
@@ -0,0 +1,16 @@
+package edu.cit.tooltrack.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class sample {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello sir";
+    }
+
+}
