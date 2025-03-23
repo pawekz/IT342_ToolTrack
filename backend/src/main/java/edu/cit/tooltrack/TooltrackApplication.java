94b76222b1e066576c917ae40cd9e8fb45673ed5
Ac Espina <aeronespina0@gmail.com>
2025-03-23 18:41:49 +0800

    Added Swagger Docs and Modified Controller files to use Swagger

--- backend/src/main/java/edu/cit/tooltrack/TooltrackApplication.java
+++ backend/src/main/java/edu/cit/tooltrack/TooltrackApplication.java
@@ -1,9 +1,14 @@
 package edu.cit.tooltrack;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "TookTrack", version = "1.0", description = "API for tooltrack project")
+)
 @SpringBootApplication
 public class TooltrackApplication {
 
--- backend/src/main/java/edu/cit/tooltrack/controller/UserAuthController.java
+++ backend/src/main/java/edu/cit/tooltrack/controller/UserAuthController.java
@@ -1,30 +1,33 @@
 package edu.cit.tooltrack.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Map;
 
-
+@Tag(name = "User Authentication API", description = "Endpoints for user authentication")
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
-public class sample {
+public class UserAuthController {
 
     private Map<String, Object> data;
 
-    @GetMapping("/login")
-    public void initiateGoogleLogin(HttpServletResponse response) throws IOException {
+    @Operation(summary = "Allow Google login", description = "uses the google api for login")
+    @GetMapping("/googlelogin")
+    public void initiateGoogleLogin(
+            HttpServletResponse response) throws IOException {
         response.sendRedirect("/oauth2/authorize/google");
     }
 
+    @Operation(summary = "google api test", description = "this test the google api works")
     @GetMapping("/user-info")
     public Map<String, Object> getUser (@AuthenticationPrincipal OAuth2User principal){
         data = principal.getAttributes();
